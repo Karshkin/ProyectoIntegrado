34a70bdd9057fb6be5dd063d52cad3b920905b16
Sergio <sirgiona@gmail.com>
2017-02-06 13:22:34 +0100

    Arreglando error de carpetas

---
+++ var/cache/dev/twig/a9/a90bdbc3a1635e039849bf0eb6d3f11434f67c2f1fb4246735af16a335edfa59.php
@@ -0,0 +1,40 @@
+<?php
+
+/* PIGBundle:Default:index.html.twig */
+class __TwigTemplate_93767be4a3b743381a7cf4d35ba3a6170ecf3ddd7a5466d9cf756f97e1eb9803 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_9b2a04d21215b3ebbd795a2c143d4f80c754c90bcb10a6e858ccd79472b82df7 = $this->env->getExtension("native_profiler");
+        $__internal_9b2a04d21215b3ebbd795a2c143d4f80c754c90bcb10a6e858ccd79472b82df7->enter($__internal_9b2a04d21215b3ebbd795a2c143d4f80c754c90bcb10a6e858ccd79472b82df7_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "PIGBundle:Default:index.html.twig"));
+
+        // line 1
+        echo "Hello World!
+";
+        
+        $__internal_9b2a04d21215b3ebbd795a2c143d4f80c754c90bcb10a6e858ccd79472b82df7->leave($__internal_9b2a04d21215b3ebbd795a2c143d4f80c754c90bcb10a6e858ccd79472b82df7_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "PIGBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 1,);
+    }
+}
+/* Hello World!*/
+/* */
